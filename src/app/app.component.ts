19bdb26bc40a830643dda66190573442e3650139
Chester Jonathan Tayag <tayagchesterjonathan@gmail.com>
2025-03-02 17:36:06 +0800

    Version 0.1  - Initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Import components
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+// Import RouterModule for working navigation
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import components
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    { path : '', title : 'CJTayag | Home', component : HomeComponent },
+    { path : 'about', title : 'CJTayag | About', component : AboutComponent },
+    { path : 'projects', title : 'CJTayag | Projects', component : ProjectsComponent },
+    { path : 'contact', title : 'CJTayag | Contact', component : ContactComponent }
+];
